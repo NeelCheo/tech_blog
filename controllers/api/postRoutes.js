5f39d5482ce17d2e58c5342186f87886a858d981
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-05-13 23:11:30 -0400

    made it so you can edit and deleted posts in the dashboard

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -38,17 +38,27 @@ router.post('/', withAuth, async (req, res) => {
 
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    const updatedPost = await Post.update({
-      ...req.body
-    }, 
-    {where:{
-      id: req.params.id,
-      user_id: req.session.user_id
-    }});
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        body: req.body.body,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
 
-    res.status(200).json(updatedPost);
+    if (!postData[0]) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
